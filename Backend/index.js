b82c124ad60fb1e66d3f257f05fc7bfe06e08da2
Rohit Ashwani <rohitas@iitbhilai.ac.in>
2023-10-17 22:57:23 +0530

    Written Add , Update amd Delete Apis

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const cookieParser = require("cookie-parser");
 
-const routes_auth = require("./routes/auth");
+// const routes_auth = require("./routes/route");
 const routes_general = require("./routes/route");
 
 const app = express();
@@ -13,7 +13,6 @@ app.use(cookieParser());
 app.use(cors());
 app.use(bodyParser.json());
 
-// app.use("/auth", routes_auth);
 app.use("/", routes_general);
 
 app.listen(process.env.PORT || 8000, () => {
---
+++ Backend/routes/auth.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const router = express.Router();
+const db = require("../db");
+const Student = require("../models/student");
+
+
+  module.exports = router;
