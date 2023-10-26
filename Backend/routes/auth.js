7950fa3c4fa8db5303e5a83374b779086f20c4c8
Aayush Krishnan <ayush05012003krishn@gmail.com>
2023-10-26 22:06:05 +0530

    Permission handling for gen-secs

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -2,19 +2,11 @@ const jwt_decode = require('jwt-decode');
 const express = require("express");
 const router = express.Router();
 const mongoose = require("mongoose");
-const { restrictToPresident } = require("../middlewares");
-
-  router.post('/', function(req, res) {
-  const userinfo= req.body;
-  email = userinfo.email;
-  sub = userinfo.sub;
-  // console.log(req.body);
+const { restrictToPresident, restrictToAdmin } = require("../middlewares");
+const Student = require("../models/student");
 
 
-  });
-  
-  
-  router.get('/', restrictToPresident, function(req, res) {
+router.get('/', restrictToPresident, function(req, res) {
     
     try{
     const jwtToken = req.headers.authorization;
@@ -23,18 +15,20 @@ const { restrictToPresident } = require("../middlewares");
  
     const { username, password } = req.DB_credentials;
     const dbUri = `mongodb+srv://${username}:${password}@cosa-database.xypqv4j.mongodb.net/?retryWrites=true&w=majority`;
-
-// Connect to the database
     mongoose.connect(dbUri, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     })
-    .then(() => {
+    .then(async () => {
     console.log('Connected to MongoDB234');
+    console.log("done");
+    return res
+    .status(201)
+    .json({ success: true, message: "Student Added Successfully" });
     })
     .catch((error) => {
     console.error('MongoDB connection error:', error);
-    });
+    })
     
 } catch(error){
   return res
@@ -42,6 +36,119 @@ const { restrictToPresident } = require("../middlewares");
 }
   }
  ); 
+
+ 
+ router.post('/add', restrictToPresident, async (req, res) => {
+
+  try {
+    const jwtToken = req.headers.authorization;
+    const user = JSON.parse(req.headers['user-details']);
+    const decoded = jwt_decode(jwtToken);
+    const { username, password } = req.DB_credentials;
+      const student = new Student({
+        name: req.body.name,
+        ID_No: req.body.ID_No,
+        Program: req.body.Program,
+        discipline: req.body.discipline,
+        pos_res: req.body.pos_res,
+        add_year: req.body.add_year,
+        achievements: req.body.achievements
+      });
+      const dbUri = `mongodb+srv://${username}:${password}@cosa-database.xypqv4j.mongodb.net/?retryWrites=true&w=majority`;
+      mongoose.connect(dbUri, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        })
+        .then(async () => {
+        
+        await student.save();
+        mongoose.connection.close();
+        console.log('MongoDB connection closed');
+          return res
+          .status(201)
+          .json({ success: true, message: "Student Added Successfully" })
+        
+        })
+        .catch((error) => {
+        console.error('MongoDB connection error:', error);
+        })
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ success: false, message: "process failed" });
+    }
+});
+
+
+router.post('/remove', restrictToPresident, async (req, res) => {
+  try {
+    
+    const jwtToken = req.headers.authorization;
+    const user = JSON.parse(req.headers['user-details']);
+    const decoded = jwt_decode(jwtToken);
+ 
+    const { username, password } = req.DB_credentials;
+
+    const dbUri = `mongodb+srv://${username}:${password}@cosa-database.xypqv4j.mongodb.net/?retryWrites=true&w=majority`;
+    mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      })
+      .then(async() => {
+      console.log('Connected to MongoDB234');
+      const student = await Student.findOne({ID_No:req.body.ID_No});
+      await Student.findByIdAndDelete((student._id));
+      mongoose.connection.close();
+      return res.status(200).json({ success: true, message: "Student Deleted Successfully" });
+      })
+      .catch((error) => {
+      console.error('MongoDB connection error:', error);
+      });
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ success: false, message: "process failed" });
+    }
+});
+
+
+router.post('/update', restrictToAdmin, async (req, res) => {
+  try {
+
+    const jwtToken = req.headers.authorization;
+    const user = JSON.parse(req.headers['user-details']);
+    const decoded = jwt_decode(jwtToken);
+    const student = await Student.findOne({ID_No:req.body.ID_No});
+    const { username, password } = req.DB_credentials;
+
+    const dbUri = `mongodb+srv://${username}:${password}@cosa-database.xypqv4j.mongodb.net/?retryWrites=true&w=majority`;
+    mongoose.connect(dbUri, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      }).then(async() => {
+        
+        console.log(student._id);
+        await Student.findByIdAndUpdate(student._id , {
+          name: req.body.name,
+          ID_No: req.body.ID_No,
+          Program: req.body.Program,
+          discipline: req.body.discipline,
+          pos_res: req.body.pos_res,
+          add_year: req.body.add_year,
+          achievements: req.body.achievements
+        });
+          mongoose.connection.close();
+          return res.status(200).json({ success: true, message: "Data Updated Successfully" });
+      })
+      .catch((error) => {
+        console.error('MongoDB connection error:', error);
+        });
+
+
+   
+    } catch (error) {
+      console.log(error);
+      return res.status(400).json({ success: false, message: "process failed" });
+    }
+});
 
 
   module.exports = router;
