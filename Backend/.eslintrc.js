ea83faf86e4ec2b79371d413cc9dda20e444e727
Snehil Shah <snehilshah.989@gmail.com>
2024-01-22 16:55:35 +0530

    Fix Husky & ESLint Configuration
    Signed-off-by: Snehil Shah <snehilshah.989@gmail.com>

--- Backend/.eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    // Other ESLint configurations...
-  
-    rules: {
-      'no-unused-vars': ['warn', { 'vars': 'all', 'args': 'none' }],
-      // Other rules...
-    },
-  };
-  
