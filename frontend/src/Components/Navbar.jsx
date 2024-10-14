d4a925a8fd5f7504849210e6e84363b1df5c9fab
amaydixit11 <amaydixit11@gmail.com>
2024-10-15 01:43:01 +0530

    added logs, updated navbar UI

--- frontend/src/Components/Navbar.jsx
+++ frontend/src/Components/Navbar.jsx
@@ -1,37 +1,57 @@
 import React, { useContext } from "react";
-import { Button, Form, Row, Col } from "reactstrap";
+import {
+  Button,
+  Row,
+  Col,
+  Dropdown,
+  DropdownToggle,
+  DropdownMenu,
+  DropdownItem,
+} from "reactstrap";
 import { useNavigate } from "react-router-dom";
 import Search from "./Search";
 import { AdminContext } from "../App";
 import { logoutUser } from "../services/auth";
+import { FaBell, FaUserCircle } from "react-icons/fa"; // Add icons
 
 function Navbar({ setStudentDetails }) {
   const { setIsUserLoggedIn } = useContext(AdminContext);
   const navigate = useNavigate();
+  const [dropdownOpen, setDropdownOpen] = React.useState(false);
+
+  const handleLogout = async (e) => {
+    e.preventDefault();
+    await logoutUser();
+    setIsUserLoggedIn(null);
+    navigate("/login");
+  };
+
+  const toggleDropdown = () => setDropdownOpen((prevState) => !prevState);
 
   return (
     <Row className="bg-success p-2">
       <Col xs="4" className="d-flex justify-content-start">
-        <span style={{ color: "white", fontSize: "130%", marginLeft: "5px" }}>
+        <span
+          className="text-white font-weight-bold"
+          style={{ fontSize: "130%", marginLeft: "5px" }}
+        >
           Student Database
         </span>
       </Col>
       <Col xs="4" className="d-flex justify-content-center">
         <Search setStudentDetails={setStudentDetails} />
       </Col>
-      <Col xs="4" className="d-flex justify-content-end">
-        <Button
-          onClick={async (e) => {
-            e.preventDefault();
-            logoutUser();
-            setIsUserLoggedIn(null);
-            navigate("/login");
-          }}
-          href="/logout"
-          color="danger"
-        >
-          Logout
-        </Button>
+      <Col xs="4" className="d-flex justify-content-end align-items-center">
+        <FaBell className="text-white mx-2" />
+        <Dropdown isOpen={dropdownOpen} toggle={toggleDropdown}>
+          <DropdownToggle caret className="text-white">
+            <FaUserCircle />
+          </DropdownToggle>
+          <DropdownMenu>
+            <DropdownItem onClick={handleLogout}>Logout</DropdownItem>
+            {/* Add more dropdown items as needed */}
+          </DropdownMenu>
+        </Dropdown>
       </Col>
     </Row>
   );
