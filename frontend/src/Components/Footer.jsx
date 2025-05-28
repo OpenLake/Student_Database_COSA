2b1ea7b275d8570fe225e99eacd235ccaab3cf44
amaydixit11 <amaydixit11@gmail.com>
2025-05-28 23:23:25 +0530

    minor refactor

---
+++ frontend/src/Components/Footer.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="footer">
+      © 2025 Student Portal. All rights reserved.
+    </footer>
+  );
+};
+
+export default Footer;
--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,16 +1,18 @@
-import './App.css';
-import Navbar from './Components/Navbar';
-import React from 'react';
+import "./App.css";
+import Body from "./Components/Body";
+import Footer from "./Components/Footer";
+import Navbar from "./Components/Navbar";
+import React, { useState } from "react";
 
 function Home() {
-
-
-    return (
+  const [studentDetails, setStudentDetails] = useState();
+  return (
     <div>
       <Navbar />
+      <Body studentDetails={studentDetails} />
+      <Footer />
     </div>
+  );
+}
 
-);
-    }
-
-    export default Home;
+export default Home;
