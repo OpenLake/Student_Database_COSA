8fb116fc5d9748e122a90a020ff623f553ae23fe
harshitap1305 <harshitapatidar257@gmail.com>
2025-06-14 12:47:00 +0530

    updated user schema

--- Backend/models/student.js
+++ Backend/models/student.js
@@ -19,6 +19,10 @@ const userSchema = new mongoose.Schema({
     type: String,
     default: "",
   },
+  onboardingComplete: {
+    type: Boolean,
+    default: false,
+  },
 });
 
 userSchema.plugin(passportLocalMongoose);
