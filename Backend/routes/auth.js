481f69d04b7767952d2141ac4d1c1e6297bbb2f3
harshitap1305 <harshitapatidar257@gmail.com>
2025-06-22 18:50:57 +0530

    backend routes added for forgot-password and password-reset & .env.example updated

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -15,7 +15,8 @@ const {
   Achievement,
 } = require("../models/student");
 const passport = require("../models/passportConfig");
-
+const rateLimit = require("express-rate-limit");
+var nodemailer = require("nodemailer");
 // Use a single database connection
 const connectDB = async () => {
   if (
@@ -36,6 +37,12 @@ const connectDB = async () => {
   }
 };
 
+//rate limiter - for password reset try
+const forgotPasswordLimiter = rateLimit({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 5,
+  message: "Too many password reset requests. Please try again later.",
+});
 // Session Status
 router.get("/fetchAuth", function (req, res) {
   if (req.isAuthenticated()) {
@@ -168,6 +175,93 @@ router.post("/logout", (req, res, next) => {
     }
     res.send("Logout Successful");
   });
+});
+
+//routes for forgot-password
+router.post("/forgot-password", forgotPasswordLimiter, async (req, res) => {
+  try {
+    const { email } = req.body;
+    const user = await User.findOne({ username: email });
+    if (!user) {
+      return res.status(404).json({ message: "User not found" });
+    }
+    const secret = user._id + process.env.JWT_SECRET_TOKEN;
+    const token = jwt.sign({ email: email, id: user._id }, secret, {
+      expiresIn: "10m",
+    });
+    const link = `${process.env.BACKEND_URL}/auth/reset-password/${user._id}/${token}`;
+    var transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASS,
+      },
+    });
+    var mailOptions = {
+      from: process.env.EMAIL_USER,
+      to: email,
+      subject: "Password-Reset Request",
+      text: `To reset your password, click here: ${link}`,
+    };
+    transporter.sendMail(mailOptions, function (error, info) {
+      if (error) {
+        console.log(error);
+        return res.status(500).json({ message: "Error sending email" });
+      } else {
+        console.log("Email sent:", info.response);
+        return res
+          .status(200)
+          .json({ message: "Password reset link sent to your email" });
+      }
+    });
+    console.log(link);
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+//route for password reset
+router.get("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  console.log(req.params);
+  const user = await User.findOne({ _id: id });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const secret = user._id + process.env.JWT_SECRET_TOKEN;
+  try {
+    jwt.verify(token, secret);
+    return res.status(200).json({ message: "Token verified successfully" });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Invalid or expired token" });
+  }
+});
+
+router.post("/reset-password/:id/:token", async (req, res) => {
+  const { id, token } = req.params;
+  const { password } = req.body;
+  const user = await User.findOne({ _id: id });
+  if (!user) {
+    return res.status(404).json({ message: "User not found" });
+  }
+  const secret = user._id + process.env.JWT_SECRET_TOKEN;
+  try {
+    jwt.verify(token, secret);
+    user.setPassword(password, async (error) => {
+      if (error) {
+        return res.status(500).json({ message: "Error resetting password" });
+      }
+      await user.save();
+      return res
+        .status(200)
+        .json({ message: "Password has been reset successfully" });
+    });
+  } catch (error) {
+    console.log(error);
+    return res.status(400).json({ message: "Invalid or expired token" });
+  }
 });
 
 // Update user profile
