eef92cc6bddd8ea6d4112296b23c6b0b1fe5daf1
harshitap1305 <harshitapatidar257@gmail.com>
2025-07-14 22:14:27 +0530

    onboarding backend

--- Backend/routes/onboarding.js
+++ Backend/routes/onboarding.js
@@ -1,39 +1,33 @@
 const express = require("express");
 const router = express.Router();
-const { User } = require("./../models/student");
-const { Student } = require("./../models/student");
+const User = require("../models/schema");
 const { verifyToken } = require("../middlewares.js");
+
 router.post("/", verifyToken, async (req, res) => {
-  const { name, email, ID_No, add_year, Program, discipline, mobile_no } =
-    req.body;
+  const { ID_No, add_year, Program, discipline, mobile_no } = req.body;
 
   try {
-    // Check if student already exists
-    const existingStudent = await Student.findOne({ ID_No });
-    if (existingStudent) {
-      return res
-        .status(400)
-        .json({ message: "Student with this ID already exists." });
-    }
+    console.log(req.user);
+    const updatedUser = await User.findByIdAndUpdate(
+      req.user._id,
+      {
+        user_id: ID_No,
+        onboardingComplete: true,
+        personal_info: Object.assign({}, req.user.personal_info, {
+          phone: mobile_no || "",
+        }),
+        academic_info: {
+          program: Program.trim(),
+          branch: discipline,
+          batch_year: add_year,
+        },
+        role: req.user.role, // required field
+        strategy: req.user.strategy, // required field
+      },
+      { new: true, runValidators: true },
+    );
 
-    // Create student record
-    const newStudent = new Student({
-      name,
-      ID_No,
-      email,
-      Program,
-      discipline,
-      add_year,
-      mobile_no: mobile_no | "",
-    });
-    await newStudent.save();
-
-    // Update user onboarding status
-    await User.findByIdAndUpdate(req.user._id, {
-      onboardingComplete: true,
-      ID_No: ID_No,
-    });
-    console.log("Onboarding completed for user:", req.user._id);
+    console.log("Onboarding completed for user:", updatedUser._id);
     res.status(200).json({ message: "Onboarding completed successfully" });
   } catch (error) {
     console.error("Onboarding failed:", error);
