83b923d18bae656299756bca208b1d825bcbe861
harshitap1305 <harshitapatidar257@gmail.com>
2025-08-20 10:22:46 +0530

    added navigation to create org predisent dashboard

--- frontend/src/config/presidentConfig.js
+++ frontend/src/config/presidentConfig.js
@@ -168,4 +168,28 @@ export const navItems = [
       </svg>
     ),
   },
+  {
+    to: "/create-org-unit",
+    title: "Create Organizational Unit",
+    description: "Create organizational units",
+    gradientClasses:
+      "from-amber-500 to-amber-600 hover:from-amber-600 hover:to-amber-700",
+    accentBgClass: "bg-amber-700",
+    icon: (
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        className="h-6 w-6 text-white"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke="currentColor"
+      >
+        <path
+          strokeLinecap="round"
+          strokeLinejoin="round"
+          strokeWidth={2}
+          d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2"
+        />
+      </svg>
+    ),
+  },
 ];
