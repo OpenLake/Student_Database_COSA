19111d5ca111eaedea5efd754b83026887fee060
harshitap1305 <harshitapatidar257@gmail.com>
2025-10-28 10:10:49 +0530

    moving the get tenure year function in a separate file

--- backend/controllers/analyticsController.js
+++ backend/controllers/analyticsController.js
@@ -1,23 +1,7 @@
 const {User, Achievement, UserSkill, Event, Position, PositionHolder,OrganizationalUnit}=require('../models/schema');
 const mongoose = require("mongoose");
-//helper function to get the current year
-const getCurrentTenureRange= () => {
-    const now=new Date();
-    const current_year=now.getFullYear();
-    const current_month=now.getMonth(); // months are 0-indexed
+const getCurrentTenureRange = require('../utils/getTenureRange');
 
-    let startDate, endDate, tenureYearString;
-    if(current_month < 3){
-        startDate=new Date(current_year-1,3,1);
-        endDate=new Date(current_year,2,31,23,59,59);
-        tenureYearString = `${current_year - 1}-${current_year}`;
-    } else {
-        startDate=new Date(current_year,3,1);
-        endDate=new Date(current_year+1,2,31,23,59,59);
-        tenureYearString = `${current_year}-${current_year + 1}`; 
-    }
-    return {startDate, endDate, tenureYearString};
-};
 
 exports.getPresidentAnalytics= async (req,res) => {
    try {
