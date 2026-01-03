21d286d74c973deb31454a0f88713281c5c3e451
UtkarshU <umapus@iitbhilai.ac.in>
2026-01-03 12:23:18 +0530

    Create POR profile page.

--- frontend/src/Components/Profile/Profile.jsx
+++ frontend/src/Components/Profile/Profile.jsx
@@ -1,8 +1,15 @@
 import React from "react";
 import StudentProfile from "./ProfilePage";
+import { useAuth } from "../../hooks/useAuth";
+
+import PORProfile from "./PORProfile";
 
 const Profile = () => {
-  return <StudentProfile />;
+  const { userRole } = useAuth();
+
+  if (userRole === "STUDENT") return <StudentProfile />;
+
+  return <PORProfile />;
 };
 
 export default Profile;
