2761ac2640f8770808462a37b49ceb4b2d0bd331
Kumar Utkarsh <kumaru@iitbhilai.ac.in>
2026-01-26 01:59:49 +0530

    changed loading page to have a spinning animation.

--- frontend/src/Components/common/LoadingScreen.js
+++ frontend/src/Components/common/LoadingScreen.js
@@ -1,8 +1,9 @@
 import React from "react";
 
 const LoadingScreen = () => (
-  <div className="loading-container">
-    <div>Loading...</div>
+  <div className="fixed inset-0 bg-[#FDFAE2] flex flex-col items-center justify-center z-50">
+    <div className="animate-spin rounded-full h-16 w-16 border-t-4 border-b-4 border-black mb-4"></div>
+    <div className="text-xl font-semibold text-black">Loading...</div>
   </div>
 );
 
