ae0295fbbec22d2a80bf6ddb3acb881df4f26eee
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-01-30 01:25:22 +0530

    feat(validation): add zod validation for auth routes with IIT Bhilai email regex

---
+++ backend/utils/validate.js
@@ -0,0 +1,19 @@
+const zod = require("zod");
+
+const loginValidate = zod.object({
+  username: zod.string().regex(/^[a-zA-Z0-9._%+-]+@iitbhilai\.ac\.in$/i),
+  password: zod.string().min(8),
+});
+
+const registerValidate = zod.object({
+  username: zod.string().regex(/^[a-zA-Z0-9._%+-]+@iitbhilai\.ac\.in$/i),
+  password: zod.string().min(8),
+  user_id: zod.string().min(2),
+  name: zod.string().min(5),
+  role: zod.string().min(5),
+});
+
+module.exports = {
+  loginValidate,
+  registerValidate,
+};
