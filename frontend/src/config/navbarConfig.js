726344315ea22811f86d03ea079615df6218f3d3
kaushikkanduri <kaushikkanduri883@gmail.com>
2026-03-04 14:09:26 +0530

    chore(ui): revamp sidebar layout and component structure

--- frontend/src/config/navbarConfig.js
+++ frontend/src/config/navbarConfig.js
@@ -14,20 +14,27 @@ import {
   Dock,
   LayoutTemplate,
   ClipboardPaste,
+  Pencil,
+  SquareCheck,
+  FolderOpen
 } from "lucide-react";
 
 const GENSEC_COMMON_NAV = [
   { key: "dashboard", label: "Home", icon: Home },
   // { key: "cosa", label: "CoSA", icon: Users },
   { key: "announcements", label: "Announcements", icon: Megaphone },
+  { key: "certificates", label: "Certificates", icon: Dock },
   { key: "events", label: "Events", icon: Calendar },
   { key: "endorsement", label: "Endorsements", icon: Award },
   { key: "feedback", label: "Feedback", icon: ClipboardList },
   // { key: "add-event", label: "Add Event", icon: Plus },
-  { key: "por", label: "PORs", icon: UserPlus },
-  { key: "profile", label: "Profile", icon: User },
   { key: "organization", label: "Clubs", icon: Users },
-  { key: "certificates", label: "Certificates", icon: Dock },
+  { key: "por", label: "PORs", icon: UserPlus },
+  { key: "requests", label: "Requests", icon: ClipboardPaste },
+  { key: "templates", label: "Templates", icon: LayoutTemplate },
+  { key: "profile", label: "Profile", icon: User },
+
+
 ];
 
 export const NavbarConfig = {
@@ -35,16 +42,16 @@ export const NavbarConfig = {
     { key: "dashboard", label: "Home", icon: Home },
     // { key: "cosa", label: "CoSA", icon: Users },
     { key: "announcements", label: "Announcements", icon: Megaphone },
-
-    { key: "por", label: "PORs", icon: UserPlus },
-    { key: "feedback", label: "Feedback", icon: ClipboardList },
+    { key: "certificates", label: "Certificates", icon: Dock },
     { key: "events", label: "Events", icon: Calendar },
-    // { key: "add-event", label: "Add Event", icon: Plus },
-    { key: "profile", label: "Profile", icon: User },
+    { key: "feedback", label: "Feedback", icon: ClipboardList },
     { key: "organization", label: "Clubs", icon: Users },
+    { key: "por", label: "PORs", icon: UserPlus },
+    // { key: "add-event", label: "Add Event", icon: Plus },
     { key: "requests", label: "Requests", icon: ClipboardPaste },
     { key: "templates", label: "Templates", icon: LayoutTemplate },
-    { key: "certificates", label: "Certificates", icon: Dock },
+    { key: "profile", label: "Profile", icon: User },
+    
   ],
   GENSEC_SCITECH: GENSEC_COMMON_NAV,
   GENSEC_ACADEMIC: GENSEC_COMMON_NAV,
@@ -53,32 +60,35 @@ export const NavbarConfig = {
 
   CLUB_COORDINATOR: [
     { key: "dashboard", label: "Home", icon: Home },
-
     // { key: "cosa", label: "CoSA", icon: Users },
     { key: "announcements", label: "Announcements", icon: Megaphone },
-
-    { key: "por", label: "PORs", icon: UserPlus },
-    { key: "events", label: "Events", icon: Calendar },
-    // { key: "add-event", label: "Add Event", icon: Plus },
-    { key: "feedback", label: "Feedback", icon: ClipboardList },
-    { key: "profile", label: "Profile", icon: User },
-    { key: "endorsement", label: "Endorsements", icon: Award },
+    { key: "batches", label: "Batches", icon: FolderOpen },
     { key: "certificates", label: "Certificates", icon: Dock },
+    { key: "drafts", label: "Draft", icon: Pencil },
+    { key: "events", label: "Events", icon: Calendar },
+    { key: "endorsements", label: "Endorsements", icon: Award },
+    { key: "feedbacks", label: "Feedback", icon: ClipboardList },
+    { key: "pors", label: "PORs", icon: UserPlus },
+    // { key: "add-event", label: "Add Event", icon: Plus },
+    { key: "requests", label: "Requests", icon: ClipboardPaste },
+    { key: "submitted", label: "Submitted", icon: SquareCheck },
+    { key: "templates", label: "Templates", icon: LayoutTemplate },
+    { key: "profile", label: "Profile", icon: User },
+    
   ],
 
   STUDENT: [
     { key: "dashboard", label: "Home", icon: Home },
     { key: "announcements", label: "Announcements", icon: Megaphone },
-
-    { key: "profile", label: "Profile", icon: User },
+    { key: "achievements", label: "Achieve", icon: Trophy },
+    { key: "certificates", label: "Certificates", icon: Dock },
     // { key: "cosa", label: "CoSA", icon: Users },
     { key: "events", label: "Events", icon: Calendar },
     { key: "feedback", label: "Feedback", icon: MessageSquare },
     // { key: "view-feedback", label: "Feedback", icon: ClipboardList },
-    { key: "achievements", label: "Achieve", icon: Trophy },
     // { key: "view-achievements", label: "View Achievements", icon: Trophy },
-    { key: "skills", label: "Skills", icon: Star },
     { key: "por", label: "PORs", icon: ClipboardList },
-    { key: "certificates", label: "Certificates", icon: Dock },
+    { key: "skills", label: "Skills", icon: Star },
+    { key: "profile", label: "Profile", icon: User },
   ],
 };
