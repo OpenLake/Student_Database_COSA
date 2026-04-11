0bab31d4a3a3b7d44b94f714ccaa4582038f87b5
Harshita Patidar <harshitapatidar257@gmail.com>
2026-04-11 19:22:06 +0530

    Refactor RoomBookingPage to use grid configuration

--- frontend/src/pages/roomBookingPage.jsx
+++ frontend/src/pages/roomBookingPage.jsx
@@ -3,10 +3,29 @@ import Layout from "../Components/common/Layout";
 import RoomBooking from "../Components/RoomBooking";
 
 const RoomBookingPage = () => {
+  const components = {
+    RoomBooking: RoomBooking,
+  };
+
+  const gridConfig = [
+    {
+      id: "main",
+      component: "RoomBooking",
+      position: {
+        colStart: 0,
+        colEnd: 20,
+        rowStart: 0,
+        rowEnd: 16,
+      },
+    },
+  ];
+
   return (
-    <Layout headerText="Room Booking">
-      <RoomBooking />
-    </Layout>
+    <Layout
+      headerText="Room Booking"
+      gridConfig={gridConfig}
+      components={components}
+    />
   );
 };
 
